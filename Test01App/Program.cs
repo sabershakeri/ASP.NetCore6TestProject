d72eea8297396b04e8a56e52c7c00d5caf008ecf
Saber <Saber@DESKTOP-94EAGE7>
2023-04-22 14:33:52 -0700

    404 bad request sample

--- Test01App/Program.cs
+++ Test01App/Program.cs
@@ -1,9 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Server.IIS;
+using System.Text;
 
-var testbuilder = builder.Build();
+var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) =>
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("Hello");
+        await context.Response.WriteAsync("World");
+
+});
 
 app.Run();
